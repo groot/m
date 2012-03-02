1df88fa2f7dc857dcbb0587a61617c3f0c3b2bdb
Tejo <tejo81@gmail.com>
2012-03-02 13:19:30 +0100

    first commit

---
+++ m.Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using m;
+
+namespace m.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var program = new Program();
+
+            Assert.IsTrue(program.Hello() == "Hello");
+        }
+    }
+}
---
+++ m/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace m
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
